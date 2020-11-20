99609e3d7df54f43b9eecb58166d29e0632364d0
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2020-11-20 10:20:56 -0500

    Updated linked list implementation
    Just started with the implementation, more to work on

--- linked-list/linked-list.c
+++ linked-list/linked-list.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 #define MAX 9
-//declaration of methods
 
+//declaration of methods
+void insertion(void);
 
 //variable declaration
 
@@ -12,15 +14,14 @@ struct node
     struct node *next_addr; 
 };
 
-struct node lists[MAX];
-
-
 struct node *head;
+struct node *new_node;
 
 
 void main(void)
 {
-    int user_input;
+
+    int user_input, choice_loop  = 1;
     printf("Please enter [0]: to print values\n");
     printf("Please enter [1]: to insert\n");
     printf("Please enter [2]: to delete\n");
@@ -29,24 +30,51 @@ void main(void)
     printf("Please enter the information: ");
     scanf("%d", &user_input);
 
-    if(user_input == 0)
+    while(choice_loop)
     {
-        printf("Printing values..");
+        if(user_input == 0)
+        {
+            printf("Printing values..");
+            break;
+        }
+        
+        else if(user_input == 1)
+        {
+            printf("Insertion executing..\n");
+            insertion();
+            break;
+        }
+
+        else if(user_input == 2)
+        {
+            printf("Deletion executing");
+            break;
+        }    
+        else if(user_input == 3)
+        {
+            printf("Exiting..");
+            choice_loop = 0;
+            break;
+        }
+        else
+        {
+            printf("Please re-enter your choice as per the instructions");
+            break;
+        }
+        
+
     }
     
-    else if(user_input == 1)
-    {
-        printf("Insertion executing");
-    }
+}
 
-    else if(user_input == 2)
-    {
-        printf("Deletion executing");
-    }    
-    else if(user_input == 3)
-    {
-        printf("Exiting..");
-    }    
+void insertion(void)
+{
+    new_node = (struct node*) malloc(sizeof(struct node));
+    printf("Pointer: %p", malloc(sizeof(struct node)));
+    printf("Please enter the value to be inserted: ");
+    scanf("%d", new_node->value);
 
+    new_node->next_addr = 0;
 
+    printf("Values are entered");
 }
