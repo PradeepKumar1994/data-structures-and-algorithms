c3f467d55b0bd9c392e987f8186d8417f5061bdd
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2020-12-05 14:34:02 -0500

    Fixed bugs for data
    The inserted value in new_node->value was taking a garbage value. Fixed with &

--- linked-list/linked-list.c
+++ linked-list/linked-list.c
@@ -64,22 +64,23 @@ void insertion(void)
 {
     //Used for debugging
     /*printf("Pointer: %p", new_node);*/
-    //new_node = (struct node*) malloc(sizeof(struct node));
-    
-    new_node = (struct node*)Marshal.AllocHGlobal(sizeof(struct node)).ToPointer();
-    printf("Pointer to current node: %p", new_node);
-    printf("Pointer to next_node: %p", new_node->next_addr);
-    
-    printf("Please enter the value to be inserted: ");
-    scanf("%p", new_node->value);
-
+    new_node = (struct node*) malloc(sizeof(struct node));
+    //new_node = (struct node*)Marshal.AllocHGlobal(sizeof(struct node)).ToPointer();
+    printf("Pointer to current node: %p \n", new_node);
+    printf("Pointer to next_node: %p  \n", new_node->next_addr);
     new_node->next_addr = 0;
+
+    printf("Please enter the value to be inserted: ");
+    scanf("%d", &new_node->value);
+    printf("Inserted data is: %d \n", new_node->value);
 
     if(head == 0)
     {
         temp_node = head = new_node;
-        temp_node->value = 0;
+        //temp_node->value = 0;
         printf("Value of head: %d and value of next address %p\n", head->value, head->next_addr);
+        printf("Value of Temp_node: %d and value of next address %p\n", temp_node->value, temp_node->next_addr);
+        printf("Value of new_node: %d and value of next address %p\n", new_node->value, new_node->next_addr);
         }
     else if(temp_node == head)
     {
@@ -87,7 +88,8 @@ void insertion(void)
         printf("Head next address: %p \n", head->next_addr);
         temp_node = new_node;
         temp_node->next_addr = 0;
-        printf("Temp_node next address: %p \n", temp_node);
+        printf("Temp_node current address: %p \n", temp_node);
+        printf("Temp_node next address: %p \n", temp_node->next_addr);
     }
     else
     {
@@ -101,8 +103,9 @@ void insertion(void)
 void print_value(void)
 {
 
-    struct node *temp = head;
-
+    struct node *temp;
+    temp = head;
+    printf("#-------------------------------------------------\n");
     while(temp)
     {
         //debugging purposes
@@ -111,6 +114,7 @@ void print_value(void)
         printf("Data of current node: %d \n", temp_node->value);
         temp = temp->next_addr;
     }
+    printf("#-------------------------------------------------");
 }
 
 
