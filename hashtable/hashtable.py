1441e412d9ca752412bfc3853eca2d25586d5e09
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2021-05-05 18:32:33 -0700

    Resloved EOL Issue and Added linkedlist to hashtable
    Not complete model, still needs to go on

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,2 +1,104 @@
-<<<<<<< HEAD
-"""@date: 4/11/2021@author: pradeep"""from linkedlist import LinkedList as linkedlistclass Hashtable:    def __init__(self):        self.sizeofhash = 15        self.hash = {}        self.hashaddress = {}        for i in range(self.sizeofhash):            self.hash[i] = i            self.hashaddress[i] = i        print(self,hash)        print(self.hashaddress)            def list_(self, hashpos, value):        if(self.hash[hashpos] == hashpos):            temp = linkedlist.insert(value)            self.hash[hashpos] = temp            self.hashaddress[hashpos] = temp        else:                            temp = linkedlist.insert(value)            self.hash[hashpos].next_ = temp            self.hashaddress = temp        def print_table(self):                print(self.hash)    def insert(self, value = None):                if(value == None):            value = int(input("Please enter your value"))                def hashfunc(key):            hashpos = key % 15            return hashpos         hashpos = hashfunc(value)        self.list_(hashpos, value)    table = Hashtable()table.insert(10)table.insert(1)table.insert(3)table.insert(16)table.print_table()#linkedlist = LinkedList()#linkedlist.insert(9)#linkedlist.insert(91)#linkedlist.insert(91)#linkedlist.insert(911)#linkedlist.insert(19)#linkedlist.insert(119)#linkedlist.print_()
+"""
+@date: 4/11/2021
+@author: pradeep
+"""
+
+from linkedlist import LinkedList as linkedlist
+
+class Hashtable:
+
+    def __init__(self):
+
+        self.sizeofhash = 15
+
+        self.hash = {}
+
+        self.hashaddress = {}
+
+        print('Initializing Hash..','And Storage for Addresses..')
+
+        for i in range(self.sizeofhash):
+
+            self.hash[i] = i
+
+            self.hashaddress[i] = i
+
+        
+    def list_(self, hashpos, value):
+
+        if(self.hash[hashpos] == hashpos):
+
+            temp = linkedlist().insert(value)
+
+            print('temp: ', temp)
+
+            self.hash[hashpos] = temp
+
+            self.hashaddress[hashpos] = temp
+
+        else:
+                
+            temp = linkedlist().insert(value)
+
+            self.hash[hashpos].next_ = temp
+
+            self.hashaddress[hashpos] = temp
+    
+    def print_table(self):
+        
+        print(self.hash)
+
+    def insert(self, value = None):
+        
+        if(value == None):
+
+            value = int(input("Please enter your value"))
+        
+        def hashfunc(key):
+
+            hashpos = key % 15
+
+            return hashpos 
+
+        hashpos = hashfunc(value)
+
+        self.list_(hashpos, value)
+    
+
+    def traverse(self):
+
+        print('Printing hash address..')
+              
+        print(self.hashaddress[1])
+
+        linkedlist().print_()
+
+
+table = Hashtable()
+
+table.insert(10)
+table.insert(1)
+table.insert(3)
+table.insert(16)
+table.insert(100)
+
+
+table.print_table()
+
+table.traverse()
+
+
+
+
+
+#linkedlist = LinkedList()
+
+#linkedlist.insert(9)
+#linkedlist.insert(91)
+#linkedlist.insert(91)
+#linkedlist.insert(911)
+#linkedlist.insert(19)
+
+#linkedlist.insert(119)
+
+#linkedlist.print_()
