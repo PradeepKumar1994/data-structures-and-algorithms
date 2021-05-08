733c7b6e3d4fbce57d7b6297de7e1588be4aa9bd
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2021-05-08 05:51:46 -0700

    Resolved Hashtable LinkedList
    With this commit we should be able to execute hashtable with linkedlist, however couple more tweaks should be better

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -11,6 +11,8 @@ class Hashtable:
 
         self.sizeofhash = 15
 
+        self.head = None
+
         self.hash = {}
 
         self.hashaddress = {}
@@ -22,7 +24,6 @@ class Hashtable:
             self.hash[i] = i
 
             self.hashaddress[i] = i
-
         
     def list_(self, hashpos, value):
 
@@ -30,19 +31,23 @@ class Hashtable:
 
             temp = linkedlist().insert(value)
 
-            print('temp: ', temp)
+            self.hashaddress[hashpos] = temp
+
+            print('temp: ', temp) #This is a lesson, the temp will always be the same
 
             self.hash[hashpos] = temp
 
-            self.hashaddress[hashpos] = temp
+            print(self.hashaddress[hashpos])
 
         else:
                 
             temp = linkedlist().insert(value)
 
+            print('temp data: ',temp.data_)
+
             self.hash[hashpos].next_ = temp
 
-            self.hashaddress[hashpos] = temp
+            self.hash[hashpos] = temp
     
     def print_table(self):
         
@@ -67,11 +72,26 @@ class Hashtable:
 
     def traverse(self):
 
+        temp_traverse = self.hashaddress[10]
+
+        while(temp_traverse!= None):
+
+            print('New elements: ',temp_traverse.data_)
+
+            temp_traverse = temp_traverse.next_
+
+        print('-----')
+
         print('Printing hash address..')
               
-        print(self.hashaddress[1])
+        print(self.hash[10].__dict__)
 
-        linkedlist().print_()
+        linkedlist.print_(self.hash[10].next_)
+
+        print(self.hashaddress[10].__dict__)
+
+        linkedlist.print_(self.hash[10].next_)
+
 
 
 table = Hashtable()
@@ -81,6 +101,8 @@ table.insert(1)
 table.insert(3)
 table.insert(16)
 table.insert(100)
+table.insert(1000)
+
 
 
 table.print_table()
