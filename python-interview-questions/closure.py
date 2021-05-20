02e734e74b8e7b2f833aa63a97d40982104a2b2b
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2021-05-20 00:10:15 -0700

    Added closure

---
+++ python-interview-questions/closure.py
@@ -0,0 +1,1 @@
+#In Python you can return a function as an object - this is closuredef my_outer_function(message):    def inner_function():        print(message)    return inner_functionfunc = my_outer_function('Hi')func()func = my_outer_function('And this is closure')func()func = my_outer_function('Bye')func()
