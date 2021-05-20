389cd13d4e2758c2bdb704448d62e6319eea5015
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2021-05-20 00:28:56 -0700

    Resolve EOL issue

--- python-interview-questions/closure.py
+++ python-interview-questions/closure.py
@@ -1,1 +1,23 @@
-#In Python you can return a function as an object - this is closuredef my_outer_function(message):    def inner_function():        print(message)    return inner_functionfunc = my_outer_function('Hi')func()func = my_outer_function('And this is closure')func()func = my_outer_function('Bye')func()
+
+#In Python you can return a function as an object - this is closure
+
+def my_outer_function(message):
+
+    def inner_function():
+
+        print(message)
+
+    return inner_function
+
+
+func = my_outer_function('Hi')
+func()
+
+print('Name of the inner function: ',func.__name__)
+
+func = my_outer_function('And this is closure')
+func()
+
+
+func = my_outer_function('Bye')
+func()
