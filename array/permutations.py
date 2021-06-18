c12b711b1c949f719fd884fa853afc7a81025019
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2021-06-18 01:02:32 -0700

    Fixed EOL conversion error

--- array/permutations.py
+++ array/permutations.py
@@ -1,1 +1,32 @@
-array = []def permutations(str_):    temp = ''    for i in range(len(str_)):        try:            if(i>0):                temp = str_[-1 * len(str_):i]        except:            temp = ""        temp1 = str_[i]+str_[i+1:]+temp        if(temp1 not in array):            print(temp1)            array.append(temp1)            return permutations(temp1)    return temp1permutations('abc')
+
+
+array = []
+
+def permutations(str_):
+
+    temp = ''
+
+    for i in range(len(str_)):
+
+        try:
+
+            if(i>0):
+
+                temp = str_[-1 * len(str_):i]
+
+        except:
+
+            temp = ""
+
+        temp1 = str_[i]+str_[i+1:]+temp
+
+        if(temp1 not in array):
+
+            array.append(temp1)
+
+            return permutations(temp1)
+
+    return array
+
+
+print(permutations('abc'))
