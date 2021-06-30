07b290eada71daebd195e4c40e669f2f661b912b
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2021-06-30 15:48:38 -0700

    Fixed logic permutation

--- array/permutations.py
+++ array/permutations.py
@@ -1,32 +1,59 @@
 
+def fact(n):
 
-array = []
+    if(n == 1):
 
-def permutations(str_):
+        return n
 
-    temp = ''
+    else:
 
-    for i in range(len(str_)):
-
-        try:
-
-            if(i>0):
-
-                temp = str_[-1 * len(str_):i]
-
-        except:
-
-            temp = ""
-
-        temp1 = str_[i]+str_[i+1:]+temp
-
-        if(temp1 not in array):
-
-            array.append(temp1)
-
-            return permutations(temp1)
-
-    return array
+        return n* fact(n-1)
 
 
-print(permutations('abc'))
+class Permutations():
+
+    def __init__(self, str_):
+
+        self.original_str_ = str_
+
+        self.str_ = str_
+
+        self.queue = []
+
+        self.length_str = len(str_)
+
+        self.factorial_max = fact(self.length_str)
+
+        print(self.factorial_max)
+
+    def permutations_words(self):
+
+        if(len(self.queue) == self.factorial_max):
+
+            return self.queue
+
+        else:
+
+            if(self.str_ not in self.queue):
+
+                print(self.str_)
+
+                self.queue.append(self.str_)
+
+                str_rev = self.str_[::-1]
+
+                print(str_rev)
+
+                self.queue.append(str_rev)
+
+                self.str_ = self.str_[-1]+self.str_[:-1]
+
+                return self.permutations_words()
+
+
+
+perm = Permutations('abc')
+
+print(perm.permutations_words())
+
+print('Runtime should be log N')
