3c1d484d6a2458a7326cc89beb538fca62e22226
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2021-07-29 13:23:32 -0700

    Self is placeholder

---
+++ python-interview-questions/New folder/self-interchange.py
@@ -0,0 +1,18 @@
+
+
+#self is just a place holder
+
+class Emp():
+
+    def __init__(not_self, string_):
+
+        not_self.string_ = string_
+
+    def name(not_self):
+
+        print('Printing....', not_self.string_)
+
+
+ee = Emp('ooo')
+
+ee.name()
