f9dc5f866d7457171445efcc26d0a4b547cc5a85
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2021-09-04 01:59:04 -0700

    Added BST Insertion

---
+++ binary-search-tree/binary-search-tree.py
@@ -0,0 +1,54 @@
+class Node():
+
+	def __init__(self, data):
+		self.left = None
+		self.data = data
+		self.right = None
+
+class BinarySearchTree():
+
+	def __init__(self):
+
+        self.node = None
+        self.root = None
+
+    def insertion(self):
+
+        if(not self.root):
+
+            self.root = Node(data)
+            return 
+
+        temp_root = self.root
+        while(temp_root):
+
+        while(temp_root != None):
+            if(data > temp_root.data):
+                if(temp_root.right == None):
+                    self.node = Node(data)
+                    temp_root.right = self.node
+                    return
+                else:
+                    temp_root = temp_root.right
+            else:
+                if(temp_root.left == None):
+                    self.node = Node(data)
+                    temp_root.left = self.data
+                    return 
+                else:
+                    temp_root = temp_root.left
+                    
+
+        return "This should not be executed"
+        
+
+
+
+
+bst = BinarySearchTree()
+
+bst.insertion(5)
+bst.insertion(11)
+bst.insertion(9)
+bst.insertion(4)
+bst.insertion(1)
