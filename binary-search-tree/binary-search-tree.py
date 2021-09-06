c5159b4603045ab1ee9c4cea8e78f317688b197d
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2021-09-05 22:01:54 -0700

    Added deletion bst

--- binary-search-tree/binary-search-tree.py
+++ binary-search-tree/binary-search-tree.py
@@ -17,7 +17,7 @@ class BinarySearchTree():
         if(not self.root):
 
             self.root = Node(data)
-            return 
+            return self.root
 
         temp_root = self.root
 
@@ -40,14 +40,98 @@ class BinarySearchTree():
 
         return "This should not be executed"
         
+    def traversal(self, root):
+        #inorder
+        
+        if(root):
+
+            self.traversal(root.left)
+            print(root.data)
+            self.traversal(root.right)
+
+    def Inordersuccessor(root):
+
+        while(current):
+            if(current.left == None):
+                if(current.right):
+                    return current.data, current.right
+                else:
+                    return current.data, None
+
+            current = current
 
 
+    def deletion(self, data):
+
+        if(self.root == None):
+
+            return not None
+
+        current, previous = self.root, self.root
+
+        while(current):
+
+            if(data == current.data):
+                #case 1: no children
+                if(not current.left and not current.right):
+                    if(current == self.root):
+                        self.root = None
+                    else:
+                        if(data < previous.data):
+                            previous.left = None
+                            return None
+                        else:
+                            previous.right = None
+                            return None
+                #case 2: two children
+                elif(current.left and current.right):
+                    temp = current.data
+                    current.data, delete_node = Inordersuccesor(current.right)
+                    print("Data to be deleted {} replaced with {}".format(temp, current.data))
+                    if(delete_node):
+                        previous.left = delete_node
+                        return None
+
+                #case 3: one child
+                else:
+                    if(not current.left):
+                        if(data < previous.data):
+                            previous.left = None
+                            return None
+                        else:
+                            previous.right
+                            return None
+
+                    else:
+                        if(data < previous.data):
+                            previous.left = None
+                            return None
+                        else:
+                            previous.right = None
+                            return None
+            else:
+                previous = current
+                if(data < current.data):
+                    current = current.left
+                else:
+                    current = current.right
+
+        return "Does not exist"
 
 
 bst = BinarySearchTree()
 
-bst.insertion(5)
-bst.insertion(11)
-bst.insertion(9)
-bst.insertion(4)
-bst.insertion(1)
+root = bst.insertion(50)
+bst.insertion(30)
+bst.insertion(20)
+bst.insertion(40)
+bst.insertion(70)
+bst.insertion(60)
+bst.insertion(80)
+print(bst.deletion(1))
+print(bst.deletion(20))
+print(bst.deletion(30))
+print(bst.deletion(50))
+
+bst.traversal(root)
+
