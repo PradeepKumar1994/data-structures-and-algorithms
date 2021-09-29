90816b1e76cdf97b19849e6ef088e583704a408f
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2021-09-28 23:33:10 -0700

    Heap Sort

--- sorting/heapsort/heap-sort.py
+++ sorting/heapsort/heap-sort.py
@@ -7,27 +7,26 @@ class Heapsort():
         self.length = -1
 
     def printf(self):
-
-        for i in self.storage:
-            print('element: ',i)
-
+        return self.storage
+        
     def insertion(self, data):
 
         if(self.length == 0):
             self.storage.append(data)
             self.length = self.length + 1
+            return None
 
         self.storage.append(data)
         self.length = self.length+1
+        
         return self.sort()
         
     def sort(self):
-
+        
         inserted_index = self.length
         
-        while(inserted_index > 0):
-            if(self.storage[inserted_index] > self.storage[inserted_index]):
-                print(self.storage[inserted_index])
+        while(inserted_index > -1):
+            if(self.storage[inserted_index] > self.storage[inserted_index//2]):
                 temp = self.storage[inserted_index//2]
                 self.storage[inserted_index//2] = self.storage[inserted_index]
                 self.storage[inserted_index] = temp
@@ -44,60 +43,54 @@ class Heapsort():
         if(self.length < 1):
             return None
 
+        temp = self.storage[0]
         self.storage[0] = self.storage[self.length]
         self.storage[self.length] = temp
         self.length = self.length - 1
-        print(self.storage)
-        self._deletion()
+        return self._deletion()
 
     def _deletion(self):
+        
+        index = 0
+        left_index = (2 * index)+1
+        right_index = (2 * index)+2
+        print(' {}   {} '.format(self.storage[left_index], self.storage[right_index]))
+        while(index >= 0):
 
-        index = self.length
-        left_index = (2 * max_length)+1
-        right_index = (2 * max_index)+2
-
-        while(index <= self.length):
-
-            if(left_index > self.length):
-                return None
-
-            elif(right > self.length):
+            if(self.storage[left_index] > self.storage[right_index]):
                 max_index = left_index
 
+            elif(self.storage[left_index] <= self.storage[right_index]):
+                max_index = right_index
+
+            if(self.storage[max_index] > self.storage[index]):
+                
+                temp = self.storage[max_index]
+                self.storage[max_index] = self.storage[index]
+                self.storage[index] = temp
+                index = index - 1
             else:
-        
-                if(self.storage[left_index] > self.storage[right_index]):
-                    max_index = left_index
+                print('ARR: ',self.storage)
+                return None
 
-                elif(slef.storage[left_index] < self.storage[right_index]):
-                    max_index = right_index
-
-                else:
-                    mex_index = left_index
-
-                if(self.storage[max_index] > self.storage[index]):
-                    
-                    temp = self.storage[max_index]
-                    self.storage[max_index] = self.storage[index]
-                    self.storage[index] = temp
-                else:
-                    return None
 
     def heapsort(self):
 
         while(self.length):
-            self.delete()
-            child_parent = self.length/2
+            self.deletion()
+            print('--->',self.printf())
+            print('length of arr: ', self.length)
+            child_parent = self.length//2
             self.heapify(child_parent)
         return self.storage
 
     def heapify(self, parent_index):
 
         left_index = (2 * parent_index)+1
-        rigth_index = (2 * parent_index)+2
+        right_index = (2 * parent_index)+2
 
         if(right_index > self.length):
-            max_index = left_index
+            return None
 
         else:
 
@@ -110,21 +103,27 @@ class Heapsort():
             else:
                 max_index = left_index
 
-        if(self.storage[parent_index] < self.storage[max_index]):
-            temp = self.storage[parent_index]
-            self.storage[parent_index] = self.storage[main_index]
-            self.storage[max_index] = temp
+            if(self.storage[parent_index] < self.storage[max_index]):
+                temp = self.storage[parent_index]
+                self.storage[parent_index] = self.storage[max_index]
+                self.storage[max_index] = temp
 
-            return None
+        return None
 
     
 heapsort = Heapsort()
 
 heapsort.printf()
 heapsort.insertion(15)
+heapsort.insertion(5)
 heapsort.insertion(20)
-heapsort.insertion(7)
-heapsort.insertion(9)
+heapsort.insertion(1)
+heapsort.insertion(17)
+heapsort.insertion(10)
 heapsort.insertion(30)
 
-heapsort.printf()
+print('returning: ',heapsort.printf())
+
+heapsort.heapsort()
+
+print('returning II: ',heapsort.printf())
