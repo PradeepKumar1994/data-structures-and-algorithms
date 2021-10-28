aa5f230b3956728634cb48162229acea7589d90a
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2021-10-27 23:10:23 -0700

    Added DFS

---
+++ recursion/DFS.py
@@ -0,0 +1,37 @@
+
+'''
+Time complexity: 
+Space complexity: 
+'''
+
+def DFS(matrix):
+
+    row = len(matrix)
+    cols = len(matrix[0]) 
+    if(row < 1):
+        print(row)
+        return None
+    stack = [0]
+    for i in range(row):
+        stack = depth(matrix, i, stack)
+        if(len(stack) == cols):
+            return stack
+
+    return stack
+
+def depth(matrix, row, stack):
+    
+    if(len(stack) == len(matrix[row])):
+        return stack
+
+    for i in range(len(matrix[row])):
+        if(matrix[row][i] > 0 and i not in stack):
+            stack.append(i)
+            return depth(matrix, i, stack)
+
+    return stack
+
+
+matrix = [[0,1,0,1,0,0,0],[1,0,1,1,0,1,1],[0,1,0,1,1,1,0],[1,1,1,0,1,0,0],[0,0,1,1,0,0,1,],[0,1,1,0,0,0,0],[0,1,0,0,1,0,0]]
+print(DFS(matrix))
+print(len(matrix[0]))
