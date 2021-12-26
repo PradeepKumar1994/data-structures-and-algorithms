67c93f4cbd0255dc6b9c2600fcd943be2f7287e1
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2021-12-26 07:13:57 -0800

    ADDED TREE TRAVERSALS

---
+++ prep-work/binary_tree_traversals.py
@@ -0,0 +1,76 @@
+'''
+Time complexity: O()
+Space complexity: O(N)
+'''
+
+class Node():
+    def __init__(self,data):
+        self.data=data
+        self.left = None
+        self.right = None
+    
+class BinarySearch():
+    def __init__(self):
+        self.node = None
+        self.queue = []
+        self.root = None
+        
+    def insert(self,data):
+        if(len(self.queue) == 0):
+            self.node = Node(data)
+            self.queue.append(self.node)
+            self.root = self.node
+        else:
+            return self._insert(data)
+
+    def _insert(self, data):
+        if(self.node.left==None):
+            self.node.left = Node(data)
+            self.queue.append(self.node.left)
+        elif(self.node.right==None):
+            self.node.right = Node(data)
+            self.queue.append(self.node.right)
+            del self.queue[0]
+            self.node = self.queue[0]
+        return None
+
+    def inorder(self):
+        return self._inorder(self.root)
+
+    def _inorder(self, root):
+        if(root):
+            self._inorder(root.left)
+            print(root.data)
+            self._inorder(root.right)
+
+    def pre_order(self):
+        return self._preorder(self.root)
+
+    def _preorder(self, root):
+        if(root):
+            print(root.data)
+            self._preorder(root.left)
+            self._preorder(root.right)
+
+    def post_order(self):
+        return self._postorder(self.root)
+
+    def _postorder(self, root):
+        if(root):
+            self._postorder(root.left)
+            self._postorder(root.right)
+            print(root.data)
+
+bs = BinarySearch()
+bs.insert(1)
+bs.insert(2)
+bs.insert(3)
+bs.insert(4)
+bs.insert(5)
+bs.insert(6)
+print('Inorder: ')
+bs.inorder()
+print('Preorder: ')
+bs.pre_order()
+print('Postorder: ')
+bs.post_order()
