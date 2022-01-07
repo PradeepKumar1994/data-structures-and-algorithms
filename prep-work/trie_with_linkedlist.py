709c446e364db3487de3c22e1bfe4dcd7bcfa51c
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2022-01-07 07:16:51 -0800

    ADDED TRIE WITH LINKEDLIST

---
+++ prep-work/trie_with_linkedlist.py
@@ -0,0 +1,62 @@
+'''
+Time complexity: O()
+Space complexity: O()
+'''
+
+class Node():
+    def __init__(self, char):
+        self.char = char
+        self.hashtable = {}
+
+class Head():
+    def __init__(self):
+        self.hashtable = {}
+
+class Trie():
+    def __init__(self):
+        self.root = Head()
+
+    def insert(self, string_):
+        if(len(string_)<1):
+            return "Please enter a valid string"
+        else:
+            return self._insert(string_, self.root)
+
+    def create_node(self, char):
+        node = Node(char)
+        return node
+
+    def _insert(self, string_, root):
+        if(len(string_)>0):
+            print('string: ',string_)
+            char = string_[0]
+            string_ = string_[1:]
+            if(char in root.hashtable.keys()):
+                return self._insert(string_, root.hashtable[char])
+            else:
+                node = self.create_node(char)
+                root.hashtable[char] = node
+                return self._insert(string_, root.hashtable[char])
+
+    def find(self, string_):
+        if(len(string_)==0):
+            return "Valid string, please"
+        else:
+            return self._find(string_, self.root)
+
+    def _find(self, string_, root):
+        if(string_!=''):
+            char = string_[0]
+            string_ = string_[1:]
+            if(char in root.hashtable.keys()):
+                return self._find(string_, root.hashtable[char])
+            else:
+                return False
+        return True
+
+trie = Trie()
+trie.insert('pradeep')
+trie.insert('naren')
+trie.insert('sheshu')
+trie.insert('praneeth')
+print('--',trie.find('shesh'))
